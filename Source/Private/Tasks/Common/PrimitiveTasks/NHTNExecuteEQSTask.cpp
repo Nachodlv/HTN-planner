494617595eaa99ea770dbcd497e0d5538ea209b2
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2022-12-09 12:58:51 -0300

    Implement execute EQS task

---
+++ Source/Private/Tasks/Common/PrimitiveTasks/NHTNExecuteEQSTask.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tasks/Common/PrimitiveTasks/NHTNExecuteEQSTask.h"
+
+// UE Includes
+#include "AIController.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Bool.h"
+#include "EnvironmentQuery/EnvQuery.h"
+#include "EnvironmentQuery/EnvQueryGenerator.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+
+// NHTN Includes
+#include "Components/NHTNComponent.h"
+
+
+UNHTNExecuteEQSTask::UNHTNExecuteEQSTask()
+{
+	StoreKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UNHTNExecuteEQSTask, StoreKey));
+	StoreKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UNHTNExecuteEQSTask, StoreKey), AActor::StaticClass());
+}
+
+ENHTNTaskStatus UNHTNExecuteEQSTask::ExecuteTask(UNHTNComponent& HTNComp)
+{
+	FQueryFinishedSignature Delegate = FQueryFinishedSignature::CreateUObject(this, &UNHTNExecuteEQSTask::OnEQSFinished, &HTNComp);
+	QueryID = EQS.Execute(*HTNComp.GetAIOwner(), HTNComp.GetBlackboardComponent(), Delegate);
+	return QueryID == INDEX_NONE ? ENHTNTaskStatus::Failed : ENHTNTaskStatus::InProgress;
+}
+
+void UNHTNExecuteEQSTask::ApplyExpectedEffects(UNHTNBlackboardComponent& WorldState) const
+{
+	WorldState.AddExpectedKeyToBeSet(StoreKey.SelectedKeyName);
+}
+
+void UNHTNExecuteEQSTask::OnEQSFinished(TSharedPtr<FEnvQueryResult, ESPMode::ThreadSafe> EnvQueryResult,
+	UNHTNComponent* HTNComp)
+{
+	UBlackboardComponent* BBComp = HTNComp->GetBlackboardComponent();
+	QueryID = INDEX_NONE;
+	if (EnvQueryResult->IsSuccessful())
+	{
+		StoreKey.ResolveSelectedKey(*BBComp->GetBlackboardAsset());
+		bool bStored = true;
+		if (BBComp && BBComp->GetKeyType(BBComp->GetKeyID(StoreKey.SelectedKeyName)) == UBlackboardKeyType_Bool::StaticClass())
+		{
+			BBComp->SetValueAsBool(StoreKey.SelectedKeyName, true);
+		}
+		else
+		{
+			const UEnvQueryItemType* ItemTypeCDO = EnvQueryResult->ItemType->GetDefaultObject<UEnvQueryItemType>();
+			bStored = ItemTypeCDO->StoreInBlackboard(StoreKey, BBComp,
+				EnvQueryResult->RawData.GetData() + EnvQueryResult->Items[0].DataOffset);
+		}
+		if (ensureMsgf(bStored, TEXT("Key %s could not be stored"), *StoreKey.SelectedKeyName.ToString()))
+		{
+			FinishLatentTask(*HTNComp, ENHTNTaskStatus::Success);
+			return;
+		}
+	}
+	else if (bClearKeyOnFail)
+	{
+		BBComp->ClearValue(StoreKey.SelectedKeyName);
+	}
+	FinishLatentTask(*HTNComp, ENHTNTaskStatus::Failed);
+}
+
+void UNHTNExecuteEQSTask::AbortTask(UNHTNComponent& HTNComp)
+{
+	if (QueryID != INDEX_NONE)
+	{
+		UEnvQueryManager* QueryManager = UEnvQueryManager::GetCurrent(HTNComp.GetWorld());
+		QueryManager->AbortQuery(QueryID);
+	}
+}
---
+++ Source/Public/Tasks/Common/PrimitiveTasks/NHTNExecuteEQSTask.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+// UE Includes
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+
+// NHTN Includes
+#include "Tasks/NHTNPrimitiveTask.h"
+
+#include "NHTNExecuteEQSTask.generated.h"
+
+/** Executes an EQS query and saves the result in a blackboard key */
+UCLASS()
+class NHTN_API UNHTNExecuteEQSTask : public UNHTNPrimitiveTask
+{
+	GENERATED_BODY()
+
+public:
+	UNHTNExecuteEQSTask();
+
+	/** Executed the EQS query */
+	virtual ENHTNTaskStatus ExecuteTask(UNHTNComponent& HTNComp) override;
+
+	/** Adds the StoreKey to the expected keys to be set */
+	virtual void ApplyExpectedEffects(UNHTNBlackboardComponent& WorldState) const override;
+
+protected:
+	/** Stores the results in the StoreKey and finishes the task */
+	void OnEQSFinished(TSharedPtr<FEnvQueryResult, ESPMode::ThreadSafe> EnvQueryResult, UNHTNComponent* HTNComp);
+
+	/** Aborts the current EQS query running */
+	virtual void AbortTask(UNHTNComponent& HTNComp) override;
+
+private:
+	/** The parametrized query to be executed */
+	UPROPERTY(EditAnywhere, Category = "NHTN")
+	FEQSParametrizedQueryExecutionRequest EQS;
+
+	/** The best item wil be stored in this key */
+	UPROPERTY(EditAnywhere, Category = "NHTN")
+	FBlackboardKeySelector StoreKey;
+
+	/** Whether the key should be clear of the EQS query fails */
+	UPROPERTY(EditAnywhere, Category = "NHTN")
+	bool bClearKeyOnFail = false;
+
+	/** The current executing query */
+	int32 QueryID = INDEX_NONE;
+};
