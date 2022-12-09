6d7dca11ac77ab67a47c25ee509d606c47ab79a7
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2022-12-09 12:59:24 -0300

    Implement decorator to check if a key is set

---
+++ Source/Private/Tasks/Common/Decorators/NHTNIsBBKeySetDecorator.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tasks/Common/Decorators/NHTNIsBBKeySetDecorator.h"
+
+bool UNHTNIsBBKeySetDecorator::CalculateRawCondition(const UNHTNBlackboardComponent& WorldState) const
+{
+	const FBlackboard::FKey KeyID = WorldState.GetKeyID(BBKey.SelectedKeyName);
+	if (WorldState.IsKeyExpectedToBeSet(KeyID))
+	{
+		return true;
+	}
+	const TSubclassOf<UBlackboardKeyType> KeyType = WorldState.GetKeyType(KeyID);
+	const UBlackboardKeyType* KeyCDO = KeyType->GetDefaultObject<UBlackboardKeyType>();
+	const uint8* KeyMemory = WorldState.GetKeyRawData(BBKey.GetSelectedKeyID());
+	return KeyCDO->WrappedTestBasicOperation(WorldState, KeyMemory, EBasicKeyOperation::Set);
+}
---
+++ Source/Public/Tasks/Common/Decorators/NHTNIsBBKeySetDecorator.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Tasks/NHTNDecorator.h"
+
+#include "NHTNIsBBKeySetDecorator.generated.h"
+
+/** Checks whether a blackboard is set */
+UCLASS()
+class NHTN_API UNHTNIsBBKeySetDecorator : public UNHTNDecorator
+{
+	GENERATED_BODY()
+
+protected:
+	/** Returns whether the BBKey is set or is expected to be set */
+	virtual bool CalculateRawCondition(const UNHTNBlackboardComponent& WorldState) const override;
+	
+private:
+	/** The key that will be checked if it is set */
+	UPROPERTY(EditAnywhere, Category = "NHTN")
+	FBlackboardKeySelector BBKey;
+};
